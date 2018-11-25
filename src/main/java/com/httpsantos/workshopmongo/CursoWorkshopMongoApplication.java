90dc0351147e4f7e856b224cbc3fe106e904abae
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2018-11-25 19:00:08 -0200

    init

---
+++ src/main/java/com/httpsantos/workshopmongo/CursoWorkshopMongoApplication.java
@@ -0,0 +1,12 @@
+package com.httpsantos.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoWorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoWorkshopMongoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/httpsantos/workshopmongo/CursoWorkshopMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.httpsantos.workshopmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoWorkshopMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
