afbc33d2fea054214368684b3f2ad0290ad64a5e
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2018-11-28 23:06:06 -0200

    INSERCAO DE USUARIO COM POST

--- src/main/java/com/httpsantos/workshopmongo/services/UserService.java
+++ src/main/java/com/httpsantos/workshopmongo/services/UserService.java
@@ -7,20 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.httpsantos.workshopmongo.domain.User;
+import com.httpsantos.workshopmongo.dto.UserDTO;
 import com.httpsantos.workshopmongo.repository.UserRepository;
 import com.httpsantos.workshopmongo.services.exception.ObjectNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
